c7914d3adc3368aa2c40e0449c0df0db4d6d34c5
jazheng <59896127+jazheng1@users.noreply.github.com>
2023-02-10 23:04:03 -0500

    starter handlebars

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const expbs = require('express-handlebars');
 const port = 3000;
+
 const app = express();
+
 app.use(express.json());
 
+app.engine('handlebars', expbs.engine({
+    defaultLayout: "main"
+}));
+app.set('view engine', 'handlebars');
+app.use(express.static('views'))
+
+//routing
 app.get('/', (req, res) => {
-    res.json({test: 'ok'});
+    res.render('index');
 })
 
 app.listen(port, ()=> {
